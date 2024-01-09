bbb4fbbfd512fbb885ebc9c9d306c013c8ebc05e
Lain0x0 <lainyamato03@gmail.com>
2024-01-09 22:04:58 +0100

    The Last Touch in Simple Shell Project

--- shell_0x2.c
+++ shell_0x2.c
@@ -1,7 +1,29 @@
-rtok(NULL, " \t\n");
-    }
+#include "shell.h"
 
-    args[arg_count] = NULL;
+/**
+ * parse_input - Tokenizes input string into arguments
+ * @input: The input string to parse
+ * @args: Array to store the resulting arguments
+ */
+void parse_input(char *input, char *args[MAX_ARGS])
+{
+	int arg_count = 0;
+	char *arg = strtok(input, " \t\n");
+
+	while (arg != NULL)
+	{
+		args[arg_count++] = arg;
+
+		if (arg_count >= MAX_ARGS)
+		{
+			printf("Excessive arguments detected.\n");
+			exit(1);
+		}
+
+		arg = strtok(NULL, " \t\n");
+	}
+
+	args[arg_count] = NULL;
 }
 
 /**
@@ -10,52 +32,53 @@ rtok(NULL, " \t\n");
  */
 int main(void)
 {
-    char input[BUFFER_SIZE];
-    char *args[MAX_ARGS];
+	char input[BUFFER_SIZE];
+	char *args[MAX_ARGS];
 
-    while (1)
-    {
-        printf(":~$ ");
+	while (1)
+	{
+		printf(":~$ ");
 
-        if (fgets(input, sizeof(input), stdin) == NULL)
-        {
-            printf("Error encountered while reading input.\n");
-            exit(1);
-        }
+		if (fgets(input, sizeof(input), stdin) == NULL)
+		{
+			printf("Error encountered while reading input.\n");
+			exit(1);
+		}
 
-        input[strcspn(input, "\n")] = '\0';
+		input[strcspn(input, "\n")] = '\0';
 
-        if (strcmp(input, "end") == 0)
-        {
-            break;
-        }
+		if (strcmp(input, "end") == 0)
+		{
+			break;
+		}
 
-        parse_input(input, args);
+		parse_input(input, args);
 
-        if (args[0] != NULL)
-        {
-            pid_t pid = fork();
+		if (args[0] != NULL)
+		{
+			pid_t pid = fork();
 
-            if (pid == 0)
-            {
-                if (execvp(args[0], args) == -1)
-                {
-                    printf("Unable to execute: %s\n", args[0]);
-                    exit(1);
-                }
-            }
-            else if (pid < 0)
-            {
-                printf("Failed to create child process.\n");
-                exit(1);
-            }
-            else
-            {
-                int status;
-                waitpid(pid, &status, 0);
-            }
-        }
-    }
+			if (pid == 0)
+			{
+				if (execvp(args[0], args) == -1)
+				{
+					printf("Unable to execute: %s\n", args[0]);
+					exit(1);
+				}
+			}
+			else if (pid < 0)
+			{
+				printf("Failed to create child process.\n");
+				exit(1);
+			}
+			else
+			{
+				int status;
+				waitpid(pid, &status, 0);
+			}
+		}
+	}
 
-    return 0;
+	return (0);
 }
+
--- shell_0x9.c
+++ shell_0x9.c
@@ -6,21 +6,21 @@
  */
 void set_env_variable(char **args)
 {
-    char *variable, *value;
+	char *variable, *value;
 
-    if (args[1] == NULL || args[2] == NULL)
-    {
-        fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
-        return;
-    }
+	if (args[1] == NULL || args[2] == NULL)
+	{
+		fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
+		return;
+	}
 
-    variable = args[1];
-    value = args[2];
+	variable = args[1];
+	value = args[2];
 
-    if (setenv(variable, value, 1) != 0)
-    {
-        perror("setenv");
-    }
+	if (setenv(variable, value, 1) != 0)
+	{
+		perror("setenv");
+	}
 }
 
 /**
@@ -29,20 +29,20 @@ void set_env_variable(char **args)
  */
 void unset_env_variable(char **args)
 {
-    char *variable;
+	char *variable;
 
-    if (args[1] == NULL)
-    {
-        fprintf(stderr, "Usage: unsetenv VARIABLE\n");
-        return;
-    }
+	if (args[1] == NULL)
+	{
+		fprintf(stderr, "Usage: unsetenv VARIABLE\n");
+		return;
+	}
 
-    variable = args[1];
+	variable = args[1];
 
-    if (unsetenv(variable) != 0)
-    {
-        perror("unsetenv");
-    }
+	if (unsetenv(variable) != 0)
+	{
+		perror("unsetenv");
+	}
 }
 
 /**
@@ -50,30 +50,47 @@ void unset_env_variable(char **args)
  */
 int main(void)
 {
-    char command[100];
-    char *args[100];
-    char *token;
-    int i;
+	char command[100];
+	char *args[100];
+	char *token;
+	int i;
 
-    while (1)
-    {
-        printf(":~$ ");
+	while (1)
+	{
+		printf(":~$ ");
 
-        fgets(command, sizeof(command), stdin);
-        command[strcspn(command, "\n")] = '\0';
+		fgets(command, sizeof(command), stdin);
+		command[strcspn(command, "\n")] = '\0';
 
-        token = strtok(command, " ");
-        i = 0;
+		token = strtok(command, " ");
+		i = 0;
 
-        while (token != NULL)
-        {
-            args[i] = token;
-            token = strtok(NULL, " ");
-            i++;
-        }
+		while (token != NULL)
+		{
+			args[i] = token;
+			token = strtok(NULL, " ");
+			i++;
+		}
 
-        args[i] = NULL;
- if (args[0] == NULL)
-        {
-            continue;
-        }
+		args[i] = NULL;
+		if (args[0] == NULL)
+		{
+			continue;
+		}
+
+		if (strcmp(args[0], "setenv") == 0)
+		{
+			set_env_variable(args);
+		}
+		else if (strcmp(args[0], "unsetenv") == 0)
+		{
+			unset_env_variable(args);
+		}
+		else
+		{
+			printf("Unknown command: %s\n", args[0]);
+		}
+	}
+
+	return (0);
+}
